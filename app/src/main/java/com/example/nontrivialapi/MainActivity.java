ee0571bcdc76f0705b4e70afd1f4bd537409fce4
ajahni1016 <alee884@binghamton.edu>
2021-10-22 10:10:15 -0400

    Updated gradle and app manifest to enable arcore. Implemented the given Google class to request permission to the app's camera. Added a function in the main activity that checks if the app was granted permission to the camera before launching.

---
+++ app/src/main/java/com/example/nontrivialapi/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.nontrivialapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.nontrivialapi.CameraPermissionHelper;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        super.onRequestPermissionsResult(requestCode, permissions, results);
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+}
