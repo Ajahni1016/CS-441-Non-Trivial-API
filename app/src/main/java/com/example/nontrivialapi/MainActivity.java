1e2c1040b92de24effb4621bd8a49e90f4bfdc72
ajahni1016 <alee884@binghamton.edu>
2021-10-28 19:23:50 -0400

    Edted the sceneform function that sets up and imports the 3d model

--- app/src/main/java/com/example/nontrivialapi/MainActivity.java
+++ app/src/main/java/com/example/nontrivialapi/MainActivity.java
@@ -1,103 +1,104 @@
 package com.example.nontrivialapi;
 
-import static com.google.ar.core.ArCoreApk.InstallStatus.INSTALLED;
-import static com.google.ar.core.ArCoreApk.InstallStatus.INSTALL_REQUESTED;
-
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.nontrivialapi.CameraPermissionHelper;
+
+import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import android.net.Uri;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private GLSurfaceView cam_view;
+    private ModelRenderable modelRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cam_view = findViewById(R.id.cam_view);
 
-        maybeEnableArButton();
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.cam_view);
+        setUpModel();
+
+
     }
 
-    //Function was included with arcore
-    //Checking if ARcore is supported on the current device
-    void maybeEnableArButton() {
-        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
-        if (availability.isTransient()) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    maybeEnableArButton();
-                }
-            }, 200);
-        }
+    private void setUpModel() {
+        ModelRenderable.builder().setSource(this, RenderableSource.builder().setSource(this, Uri.parse("BaxterModel.obj"), RenderableSource.SourceType.GLB)
+                .setScale(1.0f).setRecenterMode(RenderableSource.RecenterMode.ROOT).build())
+                .setRegistryId("BaxterModel.obj").build().thenAccept(renderable->modelRenderable=renderable);
+
     }
 
-    private boolean mUserRequestedInstall = true;
-    private Session mSession;
-    private boolean installRequested;
-//    private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mSession == null) {
-            Exception exception = null;
-            String message = null;
-            try {
-                switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
-                    case INSTALL_REQUESTED:
-                        installRequested = true;
-                        return;
-                    case INSTALLED:
-                        break;
-                }
-
-//                if (!CameraPermissionHelper.hasCameraPermission(this)) {
-//                    System.out.println("Camera not being used");
-//                    CameraPermissionHelper.requestCameraPermission(this);
-//                    return;
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        if (mSession == null) {
+//            Exception exception = null;
+//            String message = null;
+//            try {
+//                switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+//                    case INSTALL_REQUESTED:
+//                        installRequested = true;
+//                        return;
+//                    case INSTALLED:
+//                        break;
 //                }
-
-                // Create the session.
-                mSession = new Session(/* context= */ this);
-            } catch (UnavailableArcoreNotInstalledException
-                    | UnavailableUserDeclinedInstallationException e) {
-                message = "Please install ARCore";
-                exception = e;
-            } catch (UnavailableApkTooOldException e) {
-                message = "Please update ARCore";
-                exception = e;
-            } catch (UnavailableSdkTooOldException e) {
-                message = "Please update this app";
-                exception = e;
-            } catch (UnavailableDeviceNotCompatibleException e) {
-                message = "This device does not support AR";
-                exception = e;
-            } catch (Exception e) {
-                message = "Failed to create AR session";
-                exception = e;
-            }
+//
+////                if (!CameraPermissionHelper.hasCameraPermission(this)) {
+////                    System.out.println("Camera not being used");
+////                    CameraPermissionHelper.requestCameraPermission(this);
+////                    return;
+////                }
+//
+//                // Create the session.
+//                mSession = new Session(/* context= */ this);
+//            } catch (UnavailableArcoreNotInstalledException
+//                    | UnavailableUserDeclinedInstallationException e) {
+//                message = "Please install ARCore";
+//                exception = e;
+//            } catch (UnavailableApkTooOldException e) {
+//                message = "Please update ARCore";
+//                exception = e;
+//            } catch (UnavailableSdkTooOldException e) {
+//                message = "Please update this app";
+//                exception = e;
+//            } catch (UnavailableDeviceNotCompatibleException e) {
+//                message = "This device does not support AR";
+//                exception = e;
+//            } catch (Exception e) {
+//                message = "Failed to create AR session";
+//                exception = e;
+//            }
 
 //            if (message != null) {
 //                messageSnackbarHelper.showError(this, message);
 //                Log.e(TAG, "Exception creating session", exception);
 //                return;
 //            }
-        }
+//        }
 
-    }
+//    }
 
 
 }
